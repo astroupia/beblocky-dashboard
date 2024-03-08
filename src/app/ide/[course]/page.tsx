9054a194c97c08f25805ae92d62264e6cb5def76
bekacru <bekacru@gmail.com>
2024-03-08 13:19:11 +0300

    url

--- src/app/ide/[course]/page.tsx
+++ src/app/ide/[course]/page.tsx
@@ -1,8 +1,9 @@
 import IdePage from "@/components/ide/ide-page";
 import { Course, Slide } from "@/hooks/user-courses";
+import { COURSE_URL } from "@/lib/constant";
 
 export default async function page({ params }: { params: { course: string } }) {
-    const courses = await fetch("https://beb-blocky-ide.vercel.app/api/v1/courses").then(async (res) => await res.json()).then(res => res.courses as Course[])
+    const courses = await fetch(COURSE_URL).then(async (res) => await res.json()).then(res => res.courses as Course[])
     const course = courses?.find(c => c._id.toString() === params.course)
     const slides: Slide[] = course?.slides ?? []
     course?.lessons.map(d => slides.push(...d.slides))

90ca7e55123f05884fa2796534e03a5e738cf3e6
astroupia <103548612+astroupia@users.noreply.github.com>
2024-10-08 22:30:26 +0300

    fix: dialogPortalProps className change

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -15,6 +15,7 @@ const DialogTrigger = DialogPrimitive.Trigger;
 // Replace with this new DialogPortal component
 const DialogPortal = ({
   children,
+  className,
   ...props
 }: DialogPrimitive.DialogPortalProps) => (
   <DialogPrimitive.Portal {...props}>{children}</DialogPrimitive.Portal>

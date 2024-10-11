3c0d9916ba3dcc071a90d4a979eea775425eb945
astroupia <103548612+astroupia@users.noreply.github.com>
2024-10-11 17:23:58 +0300

    fix: back with the sign-in button

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,18 +1,16 @@
 import { getAuth, signOut } from "firebase/auth";
-import { Button } from '@/components/ui/button';
+import { Button } from "@/components/ui/button";
 import app from "@/lib/firebase/firebase-client";
 import Link from "next/link";
 
-const auth = getAuth(app)
+const auth = getAuth(app);
 
 export default function Home() {
   return (
     <div>
       <Link href="/sign-in">
-        <Button>
-          Sign In
-        </Button>
+        <Button>Sign In</Button>
       </Link>
     </div>
-  )
+  );
 }

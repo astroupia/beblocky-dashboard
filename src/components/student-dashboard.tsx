d961934bbf770404fcfa419e294a4564c74e779e
astroupia <103548612+astroupia@users.noreply.github.com>
2024-10-17 10:34:53 +0300

    feat: implemented - accpet a class code and associate a parent or school

--- src/components/student-dashboard.tsx
+++ src/components/student-dashboard.tsx
@@ -1,26 +1,46 @@
+"use client";
+
 import { Course } from "@/hooks/user-courses";
 import { CourseCard } from "./course-card";
 import { PageHeader } from "./page-header";
-
+import { ClassCodeDialog } from "@/components/dialogs/class-code-dialog";
+import { useState } from "react";
+import { Button } from "@/components/ui/button"; // Import your custom Button
 
 export function StudentDashboard({ courses }: { courses: Course[] }) {
+  const [isDialogOpen, setDialogOpen] = useState(false);
 
-    return (
-        <div>
-            <PageHeader />
-            <div className="flex flex-col py-4 gap-2">
-                <h3 className=" text-3xl font-bold">
-                    Your Courses
-                </h3>
-                <p className=" text-sm">
-                    Course you're enrolled to
-                </p>
-                <div className="md:flex-row flex flex-col items-start gap-4">
-                    {courses.map(course => (
-                        <CourseCard course={course} progress />
-                    ))}
-                </div>
-            </div>
+  const handleOpenDialog = () => {
+    setDialogOpen(true);
+  };
+
+  const handleCloseDialog = () => {
+    setDialogOpen(false);
+  };
+
+  return (
+    <div>
+      <PageHeader />
+      <div className="flex flex-col py-4 gap-2">
+        <h3 className="text-3xl font-bold">Your Courses</h3>
+        {courses.length === 0 ? (
+          <p className="text-sm">
+            No Course Association, Please Add a Class Code
+          </p>
+        ) : (
+          <p className="text-sm">Course you're enrolled in</p>
+        )}
+        <div className="md:flex-row flex flex-col items-start gap-4">
+          {courses.map((course) => (
+            <CourseCard key={course._id} course={course} progress />
+          ))}
         </div>
-    )
+        {/* Button to open Class Code Dialog */}
+        <Button onClick={handleOpenDialog} variant="outline">
+          Add Class Code
+        </Button>
+      </div>
+      <ClassCodeDialog isOpen={isDialogOpen} onClose={handleCloseDialog} />
+    </div>
+  );
 }

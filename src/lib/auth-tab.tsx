90b7c819f0d9cdab179f594796d49c03938e2ee9
astroupia <103548612+astroupia@users.noreply.github.com>
2024-10-17 20:45:17 +0300

    type error on auth-tab component

--- src/lib/auth-tab.tsx
+++ src/lib/auth-tab.tsx
@@ -1,23 +1,31 @@
-"use client"
+"use client";
 
 import { Tabs, Tab } from "@nextui-org/react";
 import { SignUpForm } from "../components/sign-up-form";
 import { SignInForm } from "../components/sign-in-form";
 
 export function AuthTab() {
-    return (
-        <div className="flex flex-col w-max">
-            <Tabs aria-label="Options" size="lg" defaultValue="sign-in" variant="solid" radius="full" classNames={{
-                tabList: "bg-brand-green rounded-full w-[480px]",
-                tabContent: "group-data-[selected=true]:text-[#68b946] text-[#ffffff] font-bold text-xl px-10",
-            }}>
-                <Tab key="sing-in" title="Sign In">
-                    <SignInForm />
-                </Tab>
-                <Tab key="register" title="Register">
-                    <SignUpForm />
-                </Tab>
-            </Tabs>
-        </div>
-    )
+  return (
+    <div className="flex flex-col w-max">
+      <Tabs
+        aria-label="Options"
+        size="lg"
+        variant="solid"
+        radius="full"
+        selectedKey="sign-in"
+        classNames={{
+          tabList: "bg-brand-green rounded-full w-[480px]",
+          tabContent:
+            "group-data-[selected=true]:text-[#68b946] text-[#ffffff] font-bold text-xl px-10",
+        }}
+      >
+        <Tab key="sign-in" title="Sign In">
+          <SignInForm />
+        </Tab>
+        <Tab key="register" title="Register">
+          <SignUpForm />
+        </Tab>
+      </Tabs>
+    </div>
+  );
 }

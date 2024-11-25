b7557ca9196f947eb78295c790bc943426837398
astroupia <103548612+astroupia@users.noreply.github.com>
2024-11-25 22:49:24 +0300

    chore: initial commit

---
+++ middleware.ts
@@ -0,0 +1,11 @@
+import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
+
+export const { auth: middleware } = NextAuth(authConfig);
+
+export default middleware;
+
+export const config = {
+  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+  matcher: ['/((?!api|_next/static|_next/image|.png|favicon.ico).*)'],
+};

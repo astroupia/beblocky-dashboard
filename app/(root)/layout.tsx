c97763dad03fe52a6eb775f1f848b9fc53f03af2
astroupia <103548612+astroupia@users.noreply.github.com>
2024-11-26 08:00:21 +0300

    chore: folder structure

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,22 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Beblocky Admin Dashboard",
+  description: "Analytics Oriented",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <ClerkProvider>
+        <body>{children}</body>
+      </ClerkProvider>
+    </html>
+  );
+}

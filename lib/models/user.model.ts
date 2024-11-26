da3542cd9eff2c1094781a639f0954580553f73f
astroupia <103548612+astroupia@users.noreply.github.com>
2024-11-26 08:02:13 +0300

    feat: creating user model

---
+++ lib/models/user.model.ts
@@ -0,0 +1,15 @@
+import { Schema, model, models } from "mongoose";
+
+console.log("Defining User model...");
+
+const UserSchema = new Schema({
+  clerkId: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, default: "" },
+  role: { type: String, required: true, default: "customer" },
+});
+
+const User = models.User || model("User", UserSchema);
+
+export default User;

85cbb28f3a9a78d648c6b0285e3b7496016177e5
astroupia <103548612+astroupia@users.noreply.github.com>
2024-11-27 09:16:41 +0300

    fix: type error

---
+++ src/app/dashboard/admin/page.tsx
@@ -0,0 +1,12 @@
+import { AdminDashboard } from "@/components/admin-dashboard";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <AdminDashboard />
+    </div>
+  );
+};
+
+export default page;

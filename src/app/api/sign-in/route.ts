7c989e911edba85f86ba873ec1835f6e06c57844
astroupia <103548612+astroupia@users.noreply.github.com>
2024-12-03 09:47:33 +0300

    feat: webhook clerk

--- src/app/api/sign-in/route.ts
+++ src/app/api/sign-in/route.ts
@@ -7,32 +7,37 @@ import { NextRequest, NextResponse } from "next/server";
 // Init the Firebase SDK every time the server is called
 customInitApp();
 
-export async function POST(request: NextRequest) {
+export async function POST(request: NextRequest, response: NextResponse) {
   const authorization = headers().get("Authorization");
+  console.log(authorization, "Authorization");
   if (authorization?.startsWith("Bearer ")) {
     try {
       const idToken = authorization.split("Bearer ")[1];
+      console.log(idToken, "Id token");
       const decodedToken = await auth().verifyIdToken(idToken);
+      console.log(idToken, "Id token", "decoded", decodedToken);
       if (decodedToken) {
-        const expiresIn = 60 * 60 * 24 * 5 * 1000; // 5 days
+        //Generate session cookie
+        const expiresIn = 60 * 60 * 24 * 5 * 1000;
         const sessionCookie = await auth().createSessionCookie(idToken, {
           expiresIn,
         });
-        cookies().set({
+        const options = {
           name: "session",
           value: sessionCookie,
           maxAge: expiresIn,
           httpOnly: true,
           secure: true,
-        });
+        };
+        //Add the cookie to the browser
+        cookies().set(options);
         return NextResponse.json({}, { status: 200 });
       }
-    } catch (error) {
-      console.error("Error verifying ID token:", error);
-      return NextResponse.json({ error: "Unauthorized" }, { status: 403 });
+    } catch {
+      return NextResponse.json({}, { status: 403 });
     }
   }
-  return NextResponse.json({ error: "Unauthorized" }, { status: 403 });
+  return NextResponse.json({}, { status: 403 });
 }
 
 export async function GET(request: NextRequest) {

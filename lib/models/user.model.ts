7dd1e66820a0e12d1ce9ac84b78c8d35ca81c7d1
astroupia <103548612+astroupia@users.noreply.github.com>
2024-12-13 18:30:00 +0300

    feat: update User model to support role-based access and timestamps
    - Enhanced UserSchema to include a role field with enum values for "teacher" and "admin".
    - Added discriminatorKey for role-based differentiation.
    - Enabled timestamps for user records to track creation and update times.

--- lib/models/user.model.ts
+++ lib/models/user.model.ts
@@ -2,13 +2,21 @@ import { Schema, model, models } from "mongoose";
 
 console.log("Defining User model...");
 
-const UserSchema = new Schema({
-  clerkId: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  firstName: { type: String, required: true },
-  lastName: { type: String, default: "" },
-  role: { type: String, required: true, default: "customer" },
-});
+const UserSchema = new Schema(
+  {
+    clerkId: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, default: "" },
+    role: {
+      type: String,
+      enum: ["teacher", "admin"],
+      required: true,
+      default: "teacher",
+    },
+  },
+  { discriminatorKey: "role", timestamps: true }
+);
 
 const User = models.User || model("User", UserSchema);
 

4510af827cc56b1ce7813dfea1365c3fa70b40d4
astroupia <103548612+astroupia@users.noreply.github.com>
2024-12-13 18:27:49 +0300

    feat: enhance dashboard layout and user role-based rendering
    - Added imports for Link, Image, and Logo in the dashboard layout.
    - Updated Home component to fetch user role and conditionally render AdminDashboard or TeacherDashboard based on the user's role.
    - Improved error handling for user role fetching.

--- app/(root)/dashboard/layout.tsx
+++ app/(root)/dashboard/layout.tsx
@@ -1,3 +1,7 @@
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "@/public/assets/images/logo.png";
+
 export default function RootLayout({
   children,
 }: Readonly<{

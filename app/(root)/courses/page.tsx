c01c773b01e0d2598c72204c54aff74da02bcd5a
astroupia <103548612+astroupia@users.noreply.github.com>
2024-12-13 18:27:32 +0300

    feat: add CourseDashboardPage component to courses page

---
+++ app/(root)/courses/page.tsx
@@ -0,0 +1,12 @@
+import CourseDashboardPage from "@/components/courses/course-dashboard";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <CourseDashboardPage />
+    </div>
+  );
+};
+
+export default page;

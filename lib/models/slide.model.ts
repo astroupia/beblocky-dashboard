6dd48e16bc1edea5816c415e7476b27c9b58c858
astroupia <103548612+astroupia@users.noreply.github.com>
2024-12-15 16:17:04 +0300

    feat: add Slide model for slide management

---
+++ lib/models/slide.model.ts
@@ -0,0 +1,22 @@
+import { Schema, model, models } from "mongoose";
+
+const SlideSchema = new Schema(
+  {
+    courseId: { type: Schema.Types.ObjectId, ref: "Course" },
+    lessonId: { type: Schema.Types.ObjectId, ref: "Lesson" },
+    backgroundColor: { type: String, required: false },
+    color: { type: String, required: false },
+    title: { type: String, required: true },
+    titleFont: { type: String, required: false },
+    content: { type: String, required: false },
+    contentFont: { type: String, required: false },
+    startingCode: { type: String, required: false },
+    code: { type: String, required: false },
+    image: { type: String, required: false },
+  },
+  { _id: true, timestamps: true } // Enable `_id` and `timestamps` for each slide
+);
+
+const Slide = models.Slide || model("Slide", SlideSchema);
+
+export default Slide;

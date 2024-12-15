6447abd4d8fea23bd022f1cdeb92406e2818c601
astroupia <103548612+astroupia@users.noreply.github.com>
2024-12-15 16:16:45 +0300

    feat: add Lesson model for lesson management

---
+++ lib/models/lesson.model.ts
@@ -0,0 +1,15 @@
+import { Schema, model, models } from "mongoose";
+
+const LessonSchema = new Schema(
+  {
+    courseId: { type: Schema.Types.ObjectId, ref: "Course" },
+    lessonTitle: { type: String, required: true }, // Add necessary fields for lessons
+    lessonDescription: { type: String, required: false },
+    slides: [{ type: Schema.Types.ObjectId, ref: "Slide" }], // Example field for lesson resources
+  },
+  { _id: true, timestamps: true } // Enable `_id` and timestamps for each slide
+);
+
+const Lesson = models.Lesson || model("Lesson", LessonSchema);
+
+export default Lesson;

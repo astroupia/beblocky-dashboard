13ebc381ab4f15622937db42834f09b4fed35f8f
astroupia <103548612+astroupia@users.noreply.github.com>
2024-12-25 09:52:51 +0300

    refactor: remove BrandDesign component from layout and implement AuthLayout for SignIn and SignUp pages to enhance structure and styling

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { SignIn } from "@clerk/nextjs";
+import { AuthLayout } from "@/components/auth/auth-layout";
 
 const page = () => {
   return (
-    <>
-      <section className="h-dvh bg-white m-100 flex justify-center items-center">
-        <SignIn />
-      </section>
-    </>
+    <AuthLayout mode="signin">
+      <div className="w-full">
+        <div className="bg-card p-8 rounded-lg shadow-lg border border-border">
+          <h2 className="text-center text-2xl font-bold mb-6">
+            Continue Your Journey
+          </h2>
+          <SignIn></SignIn>
+        </div>
+      </div>
+    </AuthLayout>
   );
 };
 

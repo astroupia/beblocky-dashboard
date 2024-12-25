7d2d96150e2a6a5f6ef87ee87d9368722a7d4b91
astroupia <103548612+astroupia@users.noreply.github.com>
2024-12-25 15:48:10 +0300

    refactor: remove unused properties from CreateCourseParam type for improved type safety and clarity

--- types/course.ts
+++ types/course.ts
@@ -3,8 +3,6 @@ export type CreateCourseParam = {
   courseTitle: string;
   courseDescription: string;
   courseLanguage: string;
-  slides?: Array<string> | null;
-  lessons?: Array<string> | null;
   subType: "Free" | "Gold" | "Premium" | "Standard";
   status: "Active" | "Draft";
 };

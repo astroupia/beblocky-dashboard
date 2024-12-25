eb13c8c810c42b21b969310394648205581dfe62
astroupia <103548612+astroupia@users.noreply.github.com>
2024-12-25 09:48:39 +0300

    refactor: make _id optional in CreateCourseParam and UpdateCourseParam, and update subType values for clarity

--- types/course.ts
+++ types/course.ts
@@ -1,18 +1,19 @@
 export type CreateCourseParam = {
-  _id: string;
+  _id?: string;
   courseTitle: string;
   courseDescription: string;
   courseLanguage: string;
-  slides: Array<string> | null;
-  lessons: Array<string> | null;
-  subType: "F" | "G" | "P" | "S";
+  slides?: Array<string> | null;
+  lessons?: Array<string> | null;
+  subType: "Free" | "Gold" | "Premium" | "Standard";
   status: "Active" | "Draft";
 };
 
 export type UpdateCourseParam = {
+  _id?: string;
   courseTitle: string;
   courseDescription: string;
   courseLanguage: string;
-  subType: "F" | "G" | "P" | "S";
+  subType: "Free" | "Gold" | "Premium" | "Standard";
   status: "Active" | "Draft";
 };

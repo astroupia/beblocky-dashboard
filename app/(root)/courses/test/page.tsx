87fd96409de157769381a206d1055c38fd4fb53c
astroupia <103548612+astroupia@users.noreply.github.com>
2024-12-25 09:37:48 +0300

    feat: create Course Test page component to facilitate course creation testing

---
+++ app/(root)/courses/test/page.tsx
@@ -0,0 +1,13 @@
+import CourseDashboardPage from "@/components/courses/course-dashboard";
+import CreateCourseTest from "@/components/courses/create-course-test";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <CreateCourseTest />
+    </div>
+  );
+};
+
+export default page;
